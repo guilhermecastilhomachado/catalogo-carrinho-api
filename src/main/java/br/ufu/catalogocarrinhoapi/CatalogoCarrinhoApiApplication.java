e25b694c2b00af74a02bdc964043a31e1c522cd0
Guilherme Castilho Machado <guiguicastilho34@gmail.com>
2026-04-30 21:50:26 -0300

    chore: cria estrutura inicial da api de catalogo e carrinho

---
+++ src/main/java/br/ufu/catalogocarrinhoapi/CatalogoCarrinhoApiApplication.java
@@ -0,0 +1,13 @@
+package br.ufu.catalogocarrinhoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogoCarrinhoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogoCarrinhoApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/ufu/catalogocarrinhoapi/CatalogoCarrinhoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufu.catalogocarrinhoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogoCarrinhoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
